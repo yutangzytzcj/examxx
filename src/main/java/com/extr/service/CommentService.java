5eae6d7d667d68d9cca16521deba3d3d3e114e6f
liupan <liupan@cb98ddb3-8264-4ca0-bf81-3aed82736da4>
2014-12-28 05:33:40 +0000

    git-svn-id: http://218.244.144.149/svn@21 cb98ddb3-8264-4ca0-bf81-3aed82736da4

--- src/main/java/com/extr/service/CommentService.java
+++ src/main/java/com/extr/service/CommentService.java
@@ -2,12 +2,11 @@ package com.extr.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
 
 import com.extr.domain.question.Comment;
 import com.extr.util.Page;
 
-@Service
+
 public interface CommentService {
 
 	public List<Comment> getCommentByQuestionId(int questionId,Page<Comment> page);
--- src/main/java/com/extr/service/CommentServiceImpl.java
+++ src/main/java/com/extr/service/CommentServiceImpl.java
@@ -3,11 +3,13 @@ package com.extr.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.extr.domain.question.Comment;
 import com.extr.persistence.CommentMapper;
 import com.extr.util.Page;
 
+@Service("commentService")
 public class CommentServiceImpl implements CommentService {
 
 	@Autowired
